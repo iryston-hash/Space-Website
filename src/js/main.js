063af2d976f969fc1cb406adb86fa628ddd5e84b
Iryston <81931943+iryston-hash@users.noreply.github.com>
2022-02-12 04:17:38 +0200

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,15 @@ $(function(){
     $('.slider__box').slick({
         prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/icons/la.svg" alt="left slider arrow" >',
         nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/icons/ra.svg" alt="right slider arrow" >'
-    }); 
-    
+    });
+
     $(window).scroll(function(){
         if ($(this).scrollTop() > 1600) {
           $('.pageup').fadeIn();
         } else {
           $('.pageup').fadeOut();
         }
-    }); 
+    });
     $('.menu-btn').on('click', function(){
 		$('.menu__list').toggleClass('active');
     });
